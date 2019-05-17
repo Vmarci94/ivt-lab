71317e8edef24dc2e471fe1f52c4967e8aff75de
Vmarci94 <vmarci94@gmail.com>
2019-05-17 13:54:39 +0200

    71%

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -1,10 +1,10 @@
 package hu.bme.mit.spaceship;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
@@ -19,10 +19,47 @@ public class GT4500Test {
         this.ship = new GT4500(torpedoStoreMock1, torpedoStoreMock2);
     }
 
-//    @Test
-//    public void fireTorpedo_test1(){
-//
-//    }
+    @Test
+    public void fireTorpedo_All_(){
+        when(torpedoStoreMock1.isEmpty()).thenReturn(false);
+        when(torpedoStoreMock2.isEmpty()).thenReturn(true);
+        when(torpedoStoreMock1.fire(1)).thenReturn(true);
+
+        boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void test_single(){
+        when(torpedoStoreMock1.isEmpty()).thenReturn(false);
+        when(torpedoStoreMock2.isEmpty()).thenReturn(true);
+        when(torpedoStoreMock1.fire(1)).thenReturn(true);
+
+        ship.fireTorpedo(FiringMode.ALL);
+        boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void fireTorpedo_Single_BigTest(){
+        when(torpedoStoreMock1.isEmpty()).thenReturn(false);
+        when(torpedoStoreMock2.isEmpty()).thenReturn(false);
+
+        when(torpedoStoreMock1.fire(1)).thenReturn(true);
+        when(torpedoStoreMock2.fire(1)).thenReturn(true);
+
+        for(int i = 0; i<10; i++){
+            ship.fireTorpedo(FiringMode.SINGLE);
+            ship.fireTorpedo(FiringMode.SINGLE);
+        }
+
+        verify(torpedoStoreMock1, times(10)).fire(1);
+        verify(torpedoStoreMock2, times(10)).fire(1);
+
+    }
+
 
     @Test
     public void fireTorpedo_Single_Success() {
@@ -45,13 +82,32 @@ public class GT4500Test {
     @Test
     public void fireTorpedo_All_Success() {
         // Arrange
+        when(torpedoStoreMock1.isEmpty()).thenReturn(false);
+        when(torpedoStoreMock2.isEmpty()).thenReturn(true);
+
         when(torpedoStoreMock1.fire(1)).thenReturn(true);
 
         // Act
-        boolean result = ship.fireTorpedo(FiringMode.ALL);
+        ship.fireTorpedo(FiringMode.ALL);
 
         // Assert
-        verify(torpedoStoreMock1, times(0)).fire(1);
+        verify(torpedoStoreMock1, times(1)).fire(1);
+    }
+
+    @Test
+    public void fireTorpedo_All_test3(){
+        // Arrange
+        when(torpedoStoreMock1.isEmpty()).thenReturn(true);
+        when(torpedoStoreMock2.isEmpty()).thenReturn(false);
+
+        when(torpedoStoreMock2.fire(1)).thenReturn(true);
+
+        // Act
+        ship.fireTorpedo(FiringMode.ALL);
+
+        // Assert
+        verify(torpedoStoreMock2, times(1)).fire(1);
+
     }
 
 }
