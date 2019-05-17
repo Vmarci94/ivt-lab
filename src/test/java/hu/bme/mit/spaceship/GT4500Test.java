035838dfd0fa0e738d4f589b19a0a79be98eaff1
Vmarci94 <vmarci94@gmail.com>
2019-05-17 13:10:36 +0200

    success default test

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -1,40 +1,57 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.*;
-
 public class GT4500Test {
 
-  private GT4500 ship;
+    private GT4500 ship;
+    private TorpedoStore torpedoStoreMock1;
+    private TorpedoStore torpedoStoreMock2;
 
-  @BeforeEach
-  public void init(){
-    this.ship = new GT4500();
-  }
+    @BeforeEach
+    public void init() {
+        torpedoStoreMock1 = mock(TorpedoStore.class);
+        torpedoStoreMock2 = mock(TorpedoStore.class);
+        this.ship = new GT4500(torpedoStoreMock1, torpedoStoreMock2);
+    }
 
-  @Test
-  public void fireTorpedo_Single_Success(){
-    // Arrange
+//    @Test
+//    public void fireTorpedo_test1(){
+//
+//    }
 
-    // Act
-    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    @Test
+    public void fireTorpedo_Single_Success() {
+        // Arrange
+        //Itt megmondjuk, hogy ha meghívják a kimokkolt objektumnak a fire metódusát 2-es paraméterrel, akkor true-val térjen vissza.
+        when(torpedoStoreMock1.fire(1)).thenReturn(true);
+        when(torpedoStoreMock2.fire(1)).thenReturn(true);
 
-    // Assert
-    assertEquals(true, result);
-  }
+        // Act
+        // Itt hívódik meg valahogy (nézzük meg, hogyan)
+        ship.fireTorpedo(FiringMode.SINGLE);
+        ship.fireTorpedo(FiringMode.SINGLE);
 
-  @Test
-  public void fireTorpedo_All_Success(){
-    // Arrange
+        // Assert
+        // Ellenörzi, hogy meghívták-e ennek az objektumnak (torpedoStoreMock1) a fire metódusát 2-es paraméterrel legalább egyszer
+        verify(torpedoStoreMock1, times(1)).fire(1);
+        verify(torpedoStoreMock2, times(1)).fire(1);
+    }
 
-    // Act
-    boolean result = ship.fireTorpedo(FiringMode.ALL);
+    @Test
+    public void fireTorpedo_All_Success() {
+        // Arrange
+        when(torpedoStoreMock1.fire(1)).thenReturn(true);
 
-    // Assert
-    assertEquals(true, result);
-  }
+        // Act
+        boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+        // Assert
+        verify(torpedoStoreMock1, times(0)).fire(1);
+    }
 
 }
